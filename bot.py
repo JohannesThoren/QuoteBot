e9bc8f30f49a081cc58f9a2f86dad94152487fbb
Johannes Thorén <johannes@lgjt.xyz>
2022-05-02 13:43:29 +0200

    bot is working yay

--- bot.py
+++ bot.py
@@ -1,26 +1,38 @@
 import os
+import db
+import nextcord
 
-import discord
-from discord.ext import commands
 from dotenv import load_dotenv
+from nextcord.ext import commands
+from nextcord import SlashOption, Embed
 
 load_dotenv()
 
-client = discord.Client()
-bot = commands.Bot("!")
+TESTING_GUILD_ID = 884435641268572200  # Replace with your guild ID
+
+bot = commands.Bot()
 
 
 @bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print(f'We have logged in as {bot.user}')
 
 
-@bot.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.slash_command(name="quote", description="quote a person", guild_ids=[TESTING_GUILD_ID])
+async def quote(interaction: nextcord.Interaction,
+                quote: str = SlashOption(description="quote string"),
+                person: str = SlashOption(description="the person beeing quoted"),
+                year: str = SlashOption(description="the year the person said the quote")):
+    db.add_quote_to_db(quote.lower(), person.lower(), year)
+    await interaction.send(f"{interaction.user} added: ```\"{quote}\" - {person}, {year}```")
 
-    await bot.process_commands(message)
+
+@bot.slash_command(name="get_quotes", description="Get the quotes from a specific person")
+async def get_quotes(interaction: nextcord.Interaction, person: str = SlashOption(description="the person to get quotes from"),):
+    embed = nextcord.Embed(title=f"{person}", description=f"this is all quotes form {person}", color=0x000ff)
+    for (quote, person, year) in db.get_quotes_from_person(person):
+        embed.add_field(name=f"quote from {person}, {year}", value=quote, inline=False)
+    await interaction.send(embed=embed)
 
 
 bot.run(os.getenv("TOKEN"))
--- db.py
+++ db.py
@@ -1,4 +1,43 @@
 import psycopg2
+from psycopg2 import Error
 
-conn = psycopg2.connect("host=lgjt.xyz dbname=Quotebot user=postgres password=Nzxecutp1!")
-cursor = conn.cursor()
+try:
+    connection = psycopg2.connect("host=lgjt.xyz dbname=Quotebot user=postgres password=Nzxecutp1!")
+    # Create a cursor to perform database operations
+    cursor = connection.cursor()
+
+    try:
+        cursor.execute("rollback")
+        connection.commit()
+        cursor.execute("CREATE TABLE quotes (quote TEXT, person TEXT, year TEXT)")
+        connection.commit()
+    except:
+        cursor.execute("rollback")
+        connection.commit()
+        print("table already exists")
+
+    # Print PostgreSQL details
+    print("PostgreSQL server information")
+    print(connection.get_dsn_parameters(), "\n")
+    # Executing a SQL query
+    cursor.execute("SELECT version();")
+    # Fetch result
+    record = cursor.fetchone()
+    print("You are connected to - ", record, "\n")
+
+
+
+except (Exception, Error) as error:
+    print(error)
+
+def get_quotes_from_person(person):
+    cursor.execute("ROLLBACK")
+    connection.commit()
+    cursor.execute(f"SELECT * FROM quotes WHERE person = '{person.lower()}'")
+    return cursor.fetchall()
+
+def add_quote_to_db(quote, person, year):
+    cursor.execute("ROLLBACK")
+    connection.commit()
+    cursor.execute("""INSERT INTO quotes (quote, person, year) VALUES (%s, %s, %s)""", (quote, person, year))
+    connection.commit()
